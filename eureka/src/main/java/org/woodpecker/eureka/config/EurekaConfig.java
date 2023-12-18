5fa8589c4aeb0b3ae53a6094f5d347adc01fc5f7
alex <sashaeremin08051998@gmail.com>
2023-12-18 20:37:54 +0300

    add eureka

---
+++ eureka/src/main/java/org/woodpecker/eureka/config/EurekaConfig.java
@@ -0,0 +1,9 @@
+package org.woodpecker.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEurekaServer
+@Configuration
+public class EurekaConfig {
+}
