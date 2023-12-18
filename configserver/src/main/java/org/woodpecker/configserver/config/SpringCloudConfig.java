193d3dd1765b6125e4ec104982d535c6d2774baa
alex <sashaeremin08051998@gmail.com>
2023-12-18 21:50:08 +0300

    config server

---
+++ configserver/src/main/java/org/woodpecker/configserver/config/SpringCloudConfig.java
@@ -0,0 +1,9 @@
+package org.woodpecker.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigServer
+@Configuration
+public class SpringCloudConfig {
+}
